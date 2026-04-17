01a8b8a693930eab761c698f6c8349bd40477050
mazuong2k4 <162782887+imsupershy111@users.noreply.github.com>
2026-04-17 15:52:06 +0700

    Update run_analyzer.sh

--- scripts/run_analyzer.sh
+++ scripts/run_analyzer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # ====== ENV ======
-export OPENAI_API_KEY="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"
+export OPENAI_API_KEY=""
 export OPENAI_BASE_URL="https://openrouter.ai/api/v1"
-export ABUSEIPDB_API_KEY="d7e13c6422ba0b3d9405bcfba3d28edb576f6856bc15545b094aa6fee6b6edb67e066125812e6624"
-export TELEGRAM_BOT_TOKEN="8601519829:AAF_PjU06fxN8g_D6JQ9iPxhhAY75p1f9Ks"
-export TELEGRAM_CHAT_ID="6236677517"
+export ABUSEIPDB_API_KEY=""
+export TELEGRAM_BOT_TOKEN=""
+export TELEGRAM_CHAT_ID=""
 
 # ====== PATHS ======
 PROJECT_DIR="/home/imsupershy/monitoring-project/langchain_pipeline"
